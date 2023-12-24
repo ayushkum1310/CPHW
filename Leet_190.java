a4b3e36fd99869e2b0e133f2b8233e62d1d06299
Ayush kumar <103878338+ayushkum1310@users.noreply.github.com>
2023-12-24 20:56:16 +0530

    Add files via upload

---
+++ Leet_190.java
@@ -0,0 +1,22 @@
+package CPHW;
+
+public class Leet_190 {
+	public static void reverseBits(int n) {
+        String s=Integer.toString(n);
+        System.out.println(s);
+    }
+	public static void main(String[] args) {
+		String s="00000010100101000001111010011100";
+		System.out.println(s.length());
+//		String s=Integer.toString(000000101001010);
+//		StringBuilder sn=new StringBuilder(s);
+//		System.out.println(sn);
+//		StringBuilder rev=s.reverse();
+//		String rev_st=rev.toString();
+//		System.out.println(rev);
+//		int n=Integer.parseUnsignedInt(rev_st,2);
+//		System.out.println(n);
+
+	}
+
+}
