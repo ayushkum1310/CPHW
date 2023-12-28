73406e52e481d0053ef27b5d0487cacbe569a911
Ayush kumar <103878338+ayushkum1310@users.noreply.github.com>
2023-12-28 14:29:19 +0530

    Create Leet_1757.py

---
+++ Leet_1757.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    good_products = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return good_products[['product_id']]
+    
